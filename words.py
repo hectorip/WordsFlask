3cea88cde7fe5ee1ccde88b29c77ce00037e6110
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-02-17 23:59:59 -0600

    WIP

--- words.py
+++ words.py
@@ -24,12 +24,34 @@ def api_words(word):
     return jsonify(params)
 
 def get_params_for_word(word):
-    respuesta = requests.get("https://991360be.ngrok.io/definition/" + word)
 
-    respuesta = respuesta.json()
+    headers = {
+        'app_id': 'bc5402cb',
+        'app_key': 'fb1da4ae020a29281b1a9a1845c97343'
+    }
+    response = requests.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/es/" + word.lower(), headers=headers)
+
+    if response.status_code == 200:
+        response_dict = response.json()
+        results = response_dict["results"][0]
+        lexicalEntries = results["lexicalEntries"]
+        definitions = []
+        for le in lexicalEntries:
+            entries = le["entries"]
+            for entry in entries:
+                senses = entry["senses"]
+                for sense in senses:
+                    defs = sense["definitions"]
+                    for definition in defs:
+                        definitions.append(definition)
+        # print("algo")
+        # definitions = [le["entries"]["senses"][0] for le in lexicalEntries]
+    else:
+        print("ERROR!!")
+
     params = {
         "word": word,
-        "definition": respuesta.get("definition")
+        "definitions": definitions
     }
     return params
 
